54924c1932ba93aed5e5146318156e50fcbcb099
Mariano Benedettini <mariano@codexia.io>
2018-10-30 16:22:08 -0300

    Fix styles

--- App.js
+++ App.js
@@ -193,9 +193,8 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: 'column',
+    backgroundColor: 'black'
   },
   preview: {
     flex: 1,
